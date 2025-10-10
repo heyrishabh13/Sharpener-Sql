621cb0a82dce97cba52f02c85d499f3090c81571
Rishabh Kamboj <heyrishabh13@gmail.com>
2025-10-10 21:40:50 +0530

    Student Management with Sequelize

--- Student-Management/app.js
+++ Student-Management/app.js
@@ -3,10 +3,20 @@ const db = require("./utils/db-connection");
 const studentsRoute = require("./routes/studentsRoute");
 
 const app = express();
+
+// models
+const studentModel = require("./models/students");
+
 app.use(express.json());
 
 app.use("/students", studentsRoute);
 
-app.listen(3000, () => {
-  console.log("Server is running...");
-});
+db.sync()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running...");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- Student-Management/utils/db-connection.js
+++ Student-Management/utils/db-connection.js
@@ -1,34 +1,52 @@
-const mysql = require("mysql2");
+const { Sequelize } = require("sequelize");
 
-const connection = mysql.createConnection({
+const sequelize = new Sequelize("testdb", "root", "123456", {
   host: "localhost",
-  user: "root",
-  password: "123456",
-  database: "testdb",
+  dialect: "mysql",
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.log(err);
-    return;
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been establish successfully");
+  } catch (error) {
+    console.log(error);
   }
+})();
 
-  console.log("Connection created successfully.");
+module.exports = sequelize;
 
-  const query = `CREATE TABLE IF NOT EXISTS Students(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255),
-  email VARCHAR(255) UNIQUE,
-  age INT);`;
+// const mysql = require("mysql2");
 
-  connection.execute(query, (err) => {
-    if (err) {
-      console.log(err);
-      connection.end();
-      return;
-    }
-    console.log("Table created successfully");
-  });
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "123456",
+//   database: "testdb",
+// });
 
-module.exports = connection;
+// connection.connect((err) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+
+//   console.log("Connection created successfully.");
+
+//   const query = `CREATE TABLE IF NOT EXISTS Students(
+//   id INT AUTO_INCREMENT PRIMARY KEY,
+//   name VARCHAR(255),
+//   email VARCHAR(255) UNIQUE,
+//   age INT);`;
+
+//   connection.execute(query, (err) => {
+//     if (err) {
+//       console.log(err);
+//       connection.end();
+//       return;
+//     }
+//     console.log("Table created successfully");
+//   });
+// });
+
+// module.exports = connection;
